b14dd58c7c51a01ac41a7a6898d841421944af6c
Diego-Esquivias <desqui048@west-mec.org>
2025-03-25 15:17:46 -0700

    notes

---
+++ index.js
@@ -0,0 +1,4 @@
+var f_name = "Diego";
+var alive = false;
+var age = 17;
+var greeting = "Welcome to Jurassic Park";
---
+++ index.ts
@@ -0,0 +1,6 @@
+var f_name: string = "Diego"
+var alive: boolean = false
+var age: number = 17
+
+let greeting = "Welcome to Jurassic Park"
+
