d02234ea8c428c86cf5cb94c43cde4652287ffda
Diego-Esquivias <desqui048@west-mec.org>
2025-04-09 15:12:24 -0700

    notes

--- index.js
+++ index.js
@@ -7,3 +7,27 @@ function add(n1, n2) {
     return n1 + n2;
 }
 console.log(add(12000, 32000));
+// function printResult(num: number): number {
+//     console.log('Result: ' + num);
+//     return num
+// }
+const addArrow = (n1, n2) => n1 + n2;
+const printResult = (num) => {
+    console.log('Result: ' + num);
+};
+let person;
+let product;
+var Role;
+(function (Role) {
+    Role["ADMIN"] = "ADMIN";
+    Role[Role["READ_ONLY"] = 0] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 100] = "AUTHOR";
+})(Role || (Role = {}));
+let answer;
+answer = true;
+function greet(user) {
+    console.log("Hi, I am " + user.name);
+}
+function isOlder(user, checkAge) {
+    return checkAge > user.age;
+}
--- index.ts
+++ index.ts
@@ -4,8 +4,59 @@ var age: number = 17
 
 let greeting = "Welcome to Jurassic Park"
 
-function add(n1: number, n2: number) {
-    return n1 + n2
-}
+// function add(n1: number, n2: number) {
+//     return n1 + n2
+// }
 
 console.log(add(12000, 32000))
+
+// function printResult(num: number): number {
+//     console.log('Result: ' + num);
+//     return num
+// }
+
+const addArrow = (n1:number, n2:number):number => n1 + n2
+	
+const printResult = (num: number):void => {
+    console.log('Result: ' + num);
+}
+
+let person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+}
+
+let product: {
+    id: string;
+    price: number;
+    tags: string[];
+    details: {
+        title: string;
+        description: string;
+    }
+}
+
+enum Role {
+    ADMIN = 'ADMIN',
+    READ_ONLY = 0,
+    AUTHOR = 100,
+}
+
+let answer : string | number | boolean
+answer = true
+
+type Combinable = number | string;
+
+type User = { name: string; age: number };
+
+function greet(user: User) {
+    console.log("Hi, I am " + user.name);
+}
+
+function isOlder(user: User, checkAge: number) {
+    return checkAge > user.age;
+}
+
+let add: (a: number, b: number) => number;
